a4daca737740752b3bc4df9a9a942f4477628de8
stevewooo <461437874@qq.com>
2018-07-26 21:43:20 +0800

    a

--- modules/init.js
+++ modules/init.js
@@ -2,13 +2,13 @@ module.exports = ()=>{
 	const config = {
 		okcoin : {
 			base_url : "https://www.okcoin.com",
-			apikey : '',
-			secretkey : ""
+			apikey : '34865b04-5c8b-45a4-8693-7a054d0ba76d',
+			secretkey : "988331ED369A73A1F8E2929782E589A7"
 		},
 		huobi : {
 			base_url : "https://api.huobipro.com",
-			accesskey : "",
-			secretkey : "",
+			accesskey : "7ce6ae8f-cb3ebfce-9d0a0087-cc122",
+			secretkey : "84ae02c7-0d725f17-8f18257a-97049",
 			URL_HUOBI_PRO : "api.huobipro.com",
 			accounts : {
 				spot : "2133491",
