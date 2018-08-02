c3b6ed0f25dbb95b217caaf5d7ab2c114dcfac58
stevewooo <461437874@qq.com>
2018-08-02 09:20:50 +0800

    fix count

--- modules/huobi/controller/trangle/run.js
+++ modules/huobi/controller/trangle/run.js
@@ -124,7 +124,7 @@ module.exports = (swc)=>{
 		// },
 		market_price : {}, //市场价格
 		price : {}, //当前可交易价格
-		AMOUNT_PER_BUY : 30, //交易单位额度
+		AMOUNT_PER_BUY : 15, //交易单位额度
 		last_buy_time : 0, //上次交易时间
 		buy_span : 30000, //交易最短时间跨距
 		balance : {},
