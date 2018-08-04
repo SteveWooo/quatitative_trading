a64aa088f262191526993e9c40827be45b1ce97e
stevewooo <461437874@qq.com>
2018-08-04 17:58:47 +0800

    add logs values

--- modules/huobi/controller/trangle/log_balance.js
+++ modules/huobi/controller/trangle/log_balance.js
@@ -1,11 +1,33 @@
 const fs = require('fs');
+function get_values(swc, g){
+	let market = g.market;
+	let sell = g.price.sell;
+	let balance = g.balance.trade;
+	let value = {};
+	value[market['b']] = parseFloat(balance[market['b']]);
+	value[market['a']] = parseFloat(balance[market['a']] * sell[market['A']]);
+	value[market['c']] = parseFloat(balance[market['c']] * sell[market['C']]);
+
+	var all_usdt_value = 0;
+	for(var i in value){
+		all_usdt_value += value[i];
+	}
+
+	return {
+		values : value,
+		all : all_usdt_value
+	}
+}
+
 async function mail_mention(swc, g){
+	let values = get_values(swc, g);
 	let mention = "";
 	mention += "time : <br />" + (new Date());
 	mention += "<br /><br />trade :";
 	for(var i in g.balance.trade){
-		mention += "<br />" + i + ":" + g.balance.trade[i];
+		mention += "<br />" + i + " : " + g.balance.trade[i] + "<br />"+i+" Market Value : " + values.values[i];
 	}
+	mention += "<br />total : $" + values.all;
 	mention += "<br /><br />frozen:";
 	for(var i in g.balance.frozen){
 		if(g.balance.frozen[i] != 0){
