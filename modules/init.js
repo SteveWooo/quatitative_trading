c513dc7d5407d27103033e31d4e88e2633712229
461437874@qq.com <461437874@qq.com>
2018-09-02 00:13:32 +0800

    add

--- modules/init.js
+++ modules/init.js
@@ -5,6 +5,10 @@ module.exports = ()=>{
 			apikey : '34865b04-5c8b-45a4-8693-7a054d0ba76d',
 			secretkey : "988331ED369A73A1F8E2929782E589A7"
 		},
+		binance : {
+			accesskey : "xOy1CD0SuUHjMuXYf6PSayiioDgKt0jGwc785Ag8OJmNlC2GS9Y9ARukWac8YLXq"
+			secretkey : "Pv9vyIOCeLBC7I0OtqQ6gJ2i5i7m7vowLyMKVF7gIuhswmrrXSY7FM6RtIZVWZqF"
+		},
 		huobi : {
 			base_url : "https://api.huobipro.com",
 			accesskey : "7ce6ae8f-cb3ebfce-9d0a0087-cc122",
