5cffc4f7a9ba1af3617105f68ae09f3c0da0cacb
461437874@qq.com <461437874@qq.com>
2018-09-19 11:32:03 +0800

    fix

--- modules/huobi/controller/kline/analyze.js
+++ modules/huobi/controller/kline/analyze.js
@@ -1,11 +1,11 @@
 function analyze(swc, g){
 	let result = {};
 	if(g.status == "buy"){//短线在下 准备买入
-		if(g.kline.short < g.kline.long){
+		if(g.kline.short > g.kline.long){
 			result.sig = "buy";
 		}
 	} else if (g.status == "sell"){//短线在上 准备卖出 
-		if(g.kline.long < g.kline.short){
+		if(g.kline.short < g.kline.long){
 			result.sig = "sell";
 		}
 	}
