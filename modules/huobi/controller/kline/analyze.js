47b3c558bc5746590c000704d89fe557bb39117f
461437874@qq.com <461437874@qq.com>
2018-09-20 18:05:32 +0800

    fix bug

--- modules/huobi/controller/kline/analyze.js
+++ modules/huobi/controller/kline/analyze.js
@@ -1,5 +1,10 @@
 function analyze(swc, g){
 	let result = {};
+	if(!g.kline.short || !g.kline.long){
+		throw {
+			message : "kline no full"
+		}
+	}
 	if(g.status == "buy"){//短线在下 准备买入
 		if(g.kline.short > g.kline.long + 0.2){
 			result.sig = "buy";
